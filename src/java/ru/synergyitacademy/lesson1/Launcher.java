388ea8ba0bf8cb6518a3d5bcb3f46d3dd75df2a3
karandashika <Programmistka1>
2021-04-18 21:20:37 +0700

    change and add

---
+++ src/java/ru/synergyitacademy/lesson1/Launcher.java
@@ -0,0 +1,18 @@
+package ru.synergyitacademy.lesson1;
+
+public class Launcher {
+
+    /**
+     * Основная точка входа в приложение
+     *
+     * @param args список аргументов, которые можно кастомизировать
+     * @throws Exception список исключений, которые генерируются во время выполнения программы
+     */
+    public static void main(String[] args) throws Exception {
+        // пример однострочного комментария
+        /* TODO: переписать данный код
+         Выведется в консоль фраза Привет, мир!*/
+        String str = "Привет, мир!";
+        System.out.println(str);
+    }
+}
